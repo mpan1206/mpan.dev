640afe36296976cf13a634414ed9c469752acad5
minpan <minpan1206@gmail.com>
2026-07-14 17:59:03 +0800

    chore: remove temporary test files

--- scratch.js
+++
@@ -1,5 +0,0 @@
-import { create, insert, search } from '@orama/orama'
-const db = await create({ schema: { title: 'string' } })
-await insert(db, { title: 'Hello World' })
-const res = await search(db, { term: 'hello' })
-console.log(res.hits)
